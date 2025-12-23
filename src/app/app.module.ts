5f172104d31228aebd59a54567b027c357a0f20e
Andrew Heo <andrewheo1225@gmail.com>
2025-12-22 23:00:59 -0500

    SCRUM 24: Navbar Frontend  - implemented navigation bar to various pages  - functioning log out  - route guards so user can't enter without logging in properly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,17 +21,24 @@ import { EmployeeProfilesComponent } from './pages/employee-profiles/employee-pr
 import { VisaManagementComponent } from './pages/visa-management/visa-management.component';
 import { HiringManagementComponent } from './pages/hiring-management/hiring-management.component';
 import { HousingManagementComponent } from './pages/housing-management/housing-management.component';
+import { MainLayoutComponent } from './components/layout/main-layout/main-layout.component';
+import { LoginComponent } from './pages/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
-    ,
+    NavbarComponent,
     HomeComponent,
     EmployeeProfilesComponent,
     VisaManagementComponent,
     HiringManagementComponent,
-    HousingManagementComponent
+    HousingManagementComponent,
+    MainLayoutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -40,6 +47,10 @@ import { HousingManagementComponent } from './pages/housing-management/housing-m
     AppRoutingModule,
     MatToolbarModule,
     MatIconModule,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatButtonModule,
     StoreModule.forRoot({}, {})
   ],
